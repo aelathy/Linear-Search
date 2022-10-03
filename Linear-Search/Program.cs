c8e87151a5230b0ac8077a0825c8bcf5aca9a066
aelathy <112655204+aelathy@users.noreply.github.com>
2022-10-03 13:11:36 -0600

    Add files via upload

---
+++ Linear-Search/Program.cs
@@ -0,0 +1,24 @@
+﻿#nullable disable
+Console.Clear();
+
+List<Object> cars = new List<Object> { "Volvo", "BMW", "Ram", "Ford" };
+List<Object> nums = new List<Object> { 1, 2, 3, 4, 5, 6 };
+
+int index = -1;
+// Test CONTAINS list and value of i in loop
+int linearSearch(List<Object> aList, Object item)
+{
+    foreach (Object element in aList)
+    {
+        index++;
+        if (element.Equals(item))
+        {
+            return index;
+        }
+    }
+    return -1;
+}
+
+Console.WriteLine(linearSearch(nums, 4));
+Console.WriteLine(linearSearch(cars, "Ram"));
+
